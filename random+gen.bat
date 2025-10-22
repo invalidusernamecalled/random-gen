e1351734a57b71736ca64ec18135eb9d29e9cb12
invalidusernamecalled <puneetbapna@gmail.com>
2025-10-23 03:54:51 +0530

    Add files via upload

---
+++ random+gen.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+set realset=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+set numrealset=52
+set number=%~1
+set result=
+set rand=%RANDOM%
+:initialize
+:repeat
+set chrcount=0
+for /l %%i in (1,1,%number%) do (
+if !chrcount! GTR %number% goto :eof
+set /a chrcount+=1
+call :math %%i
+CALL set result=!result!%%realset:~!num!,1%%
+)
+:next
+set result="!result:~0,%number%!"
+for /f "tokens=*" %%i in (!result!) do (echo %%~i)
+goto :eof
+:math
+set syk=%time:.=%
+set syk=%syk::=%
+set /a num=(rand*!RANDOM!*(%1+1)*7919+%syk%) %% numrealset
+if !num! LSS 0 set /a num=-num
+goto :eof
