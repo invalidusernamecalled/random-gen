0842c2ae21e14e57ca2c5742b5a6efdc32df3b57
invalidusernamecalled <puneetbapna@gmail.com>
2025-10-25 23:34:19 +0530

    Add files via upload

--- random+gen.bat
+++ random+gen.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
-set realset=abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ
+set realset=abcdefghijklmnopqrstuvwxyz12345+-#@[]}{\/:',67890ABCDEFGHIJKLMNOPQRSTUVWXYZ
 set backupset=abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ
-set numrealset=62
-set numbackupset=62
+set numrealset=75
+set numbackupset=75
 set number=%~1
 set result=
 set rand=%RANDOM%
@@ -18,18 +18,16 @@ set result=!result!!test!
 )
 if !chrcount! LSS %number% goto rerun
 :next
-set result="!result:~0,%number%!"
-for /f "tokens=*" %%i in (!result!) do (echo %%~i)
+echo !result:~0,%number%!
+
+REM ;for /f "tokens=*" %%i in (!result!) do (echo %%~i)
 goto :eof
 :math
 for /f "tokens=1,4 delims=:. " %%i in ("%time%") do set /a num=(rand*!RANDOM!*(%1+1)*7919+1%%i%%j) %% numrealset
-set /a total=chrcount+numrealset
 set test=
 if !num! LSS 0 set /a num=-num
 set /a next=num+1
 CALL set "test=%%realset:~!num!,1%%"
 if "!test!" NEQ "" (for /f "tokens=1,2" %%i in ("!num! !next!") do set realset=!realset:~0,%%i!!realset:~%%j!&set /a numrealset-=1&set /a chrcount+=1)
 if "!realset!" == ""  set realset=%backupset%&set /a numrealset=numbackupset
-
 goto :eof
-REM 'Behold, The Hand of God !!!
